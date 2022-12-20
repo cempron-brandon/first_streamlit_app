b3fe5c1cbd0e74ba76c538ec4de2e9b9bed760d4
cempron-brandon <48045671+cempron-brandon@users.noreply.github.com>
2022-12-21 03:44:01 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,7 +49,7 @@ streamlit.header("View Our Fruit List - Add Your Favorites!")
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
     my_cur.execute("select * from fruit_load_list")
-    return mycur.fetchall()
+    return my_cur.fetchall()
 
 # Add a button to load the fruit
 if streamlit.button('Get Fruit List'):
